c7f517df2c48d7fb90c8e94b4026dcbce80c5376
rasoola100 <rasoola100@gmail.com>
2024-09-20 12:34:46 +0330

    Get bottomSheet

--- lib/controller/register_controller.dart
+++ lib/controller/register_controller.dart
@@ -1,10 +1,12 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:haftsara_blog/components/api_url_constant.dart';
 import 'package:haftsara_blog/components/storage_const.dart';
+import 'package:haftsara_blog/gen/assets.gen.dart';
 import 'package:haftsara_blog/services/dio_service.dart';
 import 'package:haftsara_blog/view/home_screen.dart';
 import 'package:haftsara_blog/view/register_intro.dart';
@@ -34,7 +36,8 @@ class RegisterController extends GetxController {
       'code': activeatedCodeTextEditingController.text,
       'command': 'verify'
     };
-    var response = await DioService().postMethod(map, ApiUrlConstant.postRegister);
+    var response =
+        await DioService().postMethod(map, ApiUrlConstant.postRegister);
 
     var status = response.data['response'];
 
@@ -60,7 +63,80 @@ class RegisterController extends GetxController {
     if (GetStorage().read(token) == null) {
       Get.to(() => RegisterIntro());
     } else {
-      debugPrint('logined');
+      // debugPrint('logined');
+      routeWriteBottomSheet();
     }
+  }
+
+  routeWriteBottomSheet() {
+    Get.bottomSheet(Container(
+      height: Get.height / 2,
+      decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 228, 228, 228),
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  Assets.icons.robotIcon.path,
+                  height: 40,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Text(
+                  "دانستنی‌های خود را با ما به اشتراک بگذارید",
+                  style: TextStyle(fontSize: 18, fontFamily: 'yekan'),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text("""
+یکی از عجایبی که در جهان به چشم می‌خورد تعداد موجودات زنده‌ای است که در حال زندگی کردن هستند.
+و خدایی که روزی همه این موجودات را همواره فراهم کرده و می‌کند...
+الحمدالله الرب العالمین
+"""),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () => debugPrint('write article'),
+                  child: Container(
+                    color:const Color.fromARGB(255, 228, 228, 222),
+                    child: Row(children: [
+                      Image.asset(Assets.icons.writer.path, height: 35,),
+                      const SizedBox(width: 5,),
+                      const Text('مدیریت مقاله‌ها', style: TextStyle(fontFamily: 'yekan'),)
+                    ],),
+                  ),
+                ),
+                InkWell(
+                  onTap: () => debugPrint("record podcast"),
+                  child: Container(
+                    color: const Color.fromARGB(255, 228, 228, 222),
+                    child: Row(children: [
+                      Image.asset(Assets.icons.podcast.path, height: 35,),
+                      const SizedBox(width: 5,),
+                      const Text('مدیریت پادکست‌ها', style: TextStyle(fontFamily: 'yekan'),)
+                    ],),
+                  ),
+                ),
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
